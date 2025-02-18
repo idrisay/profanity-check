994788c85af62802e862ef9338158ded19c12ea5
Idris AY <97283155+idrisay@users.noreply.github.com>
2025-02-18 11:12:39 +0100

    feat: update example

--- example.ts
+++ example.ts
@@ -4,13 +4,10 @@ const filter = new ProfanityFilter('en');
 
 // Test cases
 const testStrings = [
-  'hello world',
-  'bad word here',  // Replace 'bad' with an actual word from your badwords/en.json
+  'good',
   'ass'
 ];
 
 testStrings.forEach(text => {
   console.log(`"${text}" is${filter.isProfane(text) ? '' : ' not'} profane`);
 });
-
-console.log('----------------------------------------------------------------')
