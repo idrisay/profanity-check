87ba1e6c402b240d4ae39d120ccc5ea94378636d
Idris AY <97283155+idrisay@users.noreply.github.com>
2025-02-18 11:55:39 +0100

    fix: remove ts

--- example.js
+++ example.js
@@ -1,10 +1,13 @@
-import ProfanityFilter from ".";
+import ProfanityFilter from './index.js';
+
 const filter = new ProfanityFilter('en');
+
 // Test cases
 const testStrings = [
     'good',
     'ass'
 ];
+
 testStrings.forEach(text => {
     console.log(`"${text}" is${filter.isProfane(text) ? '' : ' not'} profane`);
 });
--- index.d.ts
+++
@@ -1,9 +0,0 @@
-declare class ProfanityFilter {
-    #private;
-    constructor(language?: string);
-    isProfane(text: string): boolean;
-    cleanText(text: string, replacement?: string): string;
-    addWord(word: string): void;
-    removeWord(word: string): void;
-}
-export default ProfanityFilter;
